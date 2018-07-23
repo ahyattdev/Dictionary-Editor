b92a7b0bfa3fcfb3cb7babae89e046be5e49a5b6
Andrew Hyatt <ahyattdev@icloud.com>
2018-07-23 18:34:11 -0400

    Initial Commit

---
+++ Spelling Editor/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Spelling Editor
+//
+//  Created by Andrew Hyatt on 7/23/18.
+//  Copyright © 2018 Andrew Hyatt. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Spelling Editor/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Spelling Editor
+//
+//  Created by Andrew Hyatt on 7/23/18.
+//  Copyright © 2018 Andrew Hyatt. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
